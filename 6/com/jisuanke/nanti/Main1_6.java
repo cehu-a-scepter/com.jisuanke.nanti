fd1142c7af9a67ffbeb37fc42642f2f15d9261af
Scepter <admin@cehu-a.com>
2018-05-29 12:32:44 +0800

    update

--- 6/com/jisuanke/nanti/Main1_6.java
+++ 6/com/jisuanke/nanti/Main1_6.java
@@ -1,5 +1,20 @@
 package com.jisuanke.nanti;
 
+import java.util.Scanner;
+/**
+ * 此class由scepter创建于20182018年5月29日下午12:14:54
+ * 通常class中的第一行代码：package *
+ * 并不存在于“计蒜客”原答案中，是IDE创建class时不得不生成的
+ * 可以，且建议删除，以免造成不必要的麻烦与误导
+ * 计蒜客的类名要求必须是Main,请自行修改
+ * 我的class命名规则为Main题库页码_当前页中的题目号码
+ * 如Main1_1，意为题库第一页第一题
+ */
 public class Main1_6 {
-
+	public static void main(String[] args) {
+		Scanner input=new Scanner(System.in);
+		int n=input.nextInt(),x=input.nextInt(),y=input.nextInt(),z=input.nextInt();
+		String name=input.nextLine();
+		int stop=input.nextInt();
+	}
 }
