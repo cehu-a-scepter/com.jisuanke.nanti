52085ae793b51f733a0f8f395ccb8e6988a6d6dc
cehu-a-scepter <36824045+cehu-a-scepter@users.noreply.github.com>
2018-06-02 20:25:17 +0800

    Update and rename Main1.java to Main00001.java

--- 1/com/jisuanke/nanti/Main00001.java
+++ 1/com/jisuanke/nanti/Main00001.java
@@ -6,7 +6,7 @@ import java.util.Scanner;//导入java.util包中的Scanner类
  * 创建者：scepter
  * 创建时间：20182018年5月31日上午9:55:53
  */
-public class Main1 {
+public class Main00001 {
 	public static void main(String[] args) {
 		Scanner input=new Scanner(System.in);//创建对象用于获取键盘输入
 		int a=input.nextInt(),b=input.nextInt(),c=input.nextInt();//定义变量a,b,c并将键盘输入的int类型值赋给变量
